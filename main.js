9fd27b00286bd7316a2224470a4695a5d4600e11
Jacques Botha <kabouter.botha@gmail.com>
2024-07-31 19:28:31 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var mySidenav = document.getElementById("mySidenav")
+function openNav() {
+    mySidenav.style.opacity = "1";
+    mySidenav.style.zIndex = "1";
+}
+
+function closeNav() {
+    mySidenav.style.opacity = "0";
+    mySidenav.style.zIndex = "0";
+}
